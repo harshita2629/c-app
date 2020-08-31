e281ac687c5db1d0ef03ad1af073e6e654b7c942
harshita2629 <54017759+harshita2629@users.noreply.github.com>
2020-08-31 16:50:10 +0530

    Update RootStackScreen.js

--- screens/RootStackScreen.js
+++ screens/RootStackScreen.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SplashScreen from './SplashScreen';
-import SignInScreen from './SignInScreen';
+import LogInScreen from './LogInScreen';
 import SignUpScreen from './SignUpScreen';
 
 const RootStack = createStackNavigator();
@@ -11,7 +11,7 @@ const RootStack = createStackNavigator();
 const RootStackScreen = ({navigation}) => (
     <RootStack.Navigator headerMode='none'>
         <RootStack.Screen name="SplashScreen" component={SplashScreen}/>
-        <RootStack.Screen name="SignInScreen" component={SignInScreen}/>
+        <RootStack.Screen name="LogInScreen" component={LogInScreen}/>
         <RootStack.Screen name="SignUpScreen" component={SignUpScreen}/>
     </RootStack.Navigator>
 );
